a1a36d2d3105e694615a2d4e80af86931844d9e8
midul9797 <mrmnso9797@gmail.com>
2023-05-30 19:53:45 +0600

    [DONE] Setup ESlint,Prettier,Husky, lint-staged

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
-const app: Application = express();
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+const app: Application = express()
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello world");
-});
-export default app;
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello world')
+})
+export default app
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,16 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config/index";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config/index'
 
 async function boostrap() {
   try {
-    await mongoose.connect(config.database_url as string);
-    console.log("DB connected");
+    await mongoose.connect(config.database_url as string)
+    console.log('DB connected')
     app.listen(config.port, () => {
-      console.log("Listening on port " + config.port);
-    });
+      console.log('Listening on port ' + config.port)
+    })
   } catch (error) {
-    console.log("Failed to connect DB" + error);
+    console.log('Failed to connect DB' + error)
   }
 }
-boostrap();
+boostrap()
